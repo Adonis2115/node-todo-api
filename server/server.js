3ad8d013eaa242d3a648eedce33fae544cf6b07d
Himanshu Pandey <himanshu21pandey@gmail.com>
2017-01-23 21:52:10 +0530

    Add post /users login route

--- server/server.js
+++ server/server.js
@@ -88,10 +88,6 @@ app.patch('/todos/:id', (req, res) => {
 	});
 });
 
-app.get('/users/me', authenticate, (req, res) => {
-	res.send(req.user);
-});
-
 app.post('/users', (req, res) => {
 	var body = _.pick(req.body, ['email', 'password']);
 	var user = new User(body);
@@ -102,6 +98,21 @@ app.post('/users', (req, res) => {
 		res.header('x-auth', token).send(user);
 	}).catch((e) => {
 		res.status(400).send(e);
+	});
+});
+
+app.get('/users/me', authenticate, (req, res) => {
+	res.send(req.user);
+});
+
+app.post('/users/login', (req, res) => {
+	var body = _.pick(req.body, ['email', 'password']);
+	User.findByCredentials(body.email, body.password).then((user) => {
+		user.generateAuthToken().then((token) => {
+			res.header('x-auth', token).send(user);
+		});
+	}).catch((e) => {
+		res.status(400).send();
 	});
 });
 
